78e9b6d33c7e30dc8543c55879f9839d33bc4277
DanieleLena <daniele.lena@yahoo.it>
2021-08-08 15:39:02 +0200

    change url

--- client/src/helpers.tsx
+++ client/src/helpers.tsx
@@ -50,5 +50,5 @@ export function useDelayUnmount(isMounted: boolean, delayTime: number) {
 
 
 
-export const invoices_url = "http://invoice-app-daniele.herokuapp.com/invoices/";
+export const invoices_url = "https://invoice-app-daniele.herokuapp.com/invoices/";
 
