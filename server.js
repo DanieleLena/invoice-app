149e3a515e79cd13a5ee57fb872eef45525f7569
DanieleLena <daniele.lena@yahoo.it>
2021-08-08 17:47:12 +0200

    Deploy on heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const mongoose = require("mongoose");
 const path = require("path");
 
-require("dotenv").config();
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
+
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true,
