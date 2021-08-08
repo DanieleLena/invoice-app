0f700a0e8a68d12f9e3acc5666fd4367f5e61d78
DanieleLena <daniele.lena@yahoo.it>
2021-08-08 15:09:14 +0200

    Changed folder structure to deploy on heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require('path');
 
 require("dotenv").config();
 
@@ -23,8 +24,17 @@ connection.once("open", () => {
 });
 
 const invoicesRouter = require('./routes/invoices');
+const { resolve } = require("path");
 
 app.use('/invoices', invoicesRouter);
+
+if(process.env.NODE_ENV  === "production") {
+  app.use(express.static("client/build"));
+
+  app.get('*', (req,res) => {
+res.sendFile(path,resolve(__dirname,'client','build','index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
